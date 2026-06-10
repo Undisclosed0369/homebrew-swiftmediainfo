6f5e744d19e2220a667aa57158f900647cf52849
Undisclosed <undisclosed0369@gmail.com>
2026-06-10 22:01:00 +0800

    Create swiftmediainfo.rb

---
+++ Casks/swiftmediainfo.rb
@@ -0,0 +1,13 @@
+cask "swiftmediainfo" do
+  version "1.4"
+
+  sha256 "f00488040829315267ac3cdfc8560566eead072ee9acbfbcd84137ea412e8c5a"
+
+  url "https://github.com/Undisclosed0369/SwiftMediaInfo/releases/download/v1.4/SwiftMediaInfo.1.4.dmg"
+
+  name "SwiftMediaInfo"
+  desc "Native macOS frontend for MediaInfo"
+  homepage "https://github.com/Undisclosed0369/SwiftMediaInfo"
+
+  app "SwiftMediaInfo.app"
+end
