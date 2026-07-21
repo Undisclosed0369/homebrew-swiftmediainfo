989352935de692db178c4eda938f6dc6641245a7
Undisclosed <undisclosed0369@gmail.com>
2026-07-21 16:01:45 +0800

    Update swiftmediainfo.rb

--- Casks/swiftmediainfo.rb
+++ Casks/swiftmediainfo.rb
@@ -1,9 +1,9 @@
 cask "swiftmediainfo" do
-  version "1.5"
+  version "1.6"
 
-  sha256 "725b71725deb5b1294d2e73bb67f25db68ab3a79165520e03f0fc23ae956c80b"
+  sha256 "546d405aba187fc173f2404f5610fc30a6fd25bce855ff1dab37418c94a0b568"
 
-  url "https://github.com/Undisclosed0369/SwiftMediaInfo/releases/download/v1.5/SwiftMediaInfo.1.5.dmg"
+  url "https://github.com/Undisclosed0369/SwiftMediaInfo/releases/download/v1.6/SwiftMediaInfo.1.6.dmg"
 
   name "SwiftMediaInfo"
   desc "Native macOS frontend for MediaInfo"
